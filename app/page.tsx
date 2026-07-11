c716f3f8d38099a72470eb6b26652593e8714b2f
Artem Chupylko <achupylko@gmail.com>
2026-07-11 07:15:03 +0000

    feat(home): add Join section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/home/Hero/Hero';
 import About from '@/components/About/About';
+import Join from '@/components/home/Join/Join';
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
       <About />
+      <Join isAuthenticated={false} />
     </main>
   );
 }
