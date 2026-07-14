05c35fea6b73aba9534fc067fea6a72642b68b77
Viktor Hnatiuk <viktorgnatiuk136@gmail.com>
2026-07-14 23:56:48 +0300

    Update page.tsx

--- app/travellers/page.tsx
+++ app/travellers/page.tsx
@@ -3,7 +3,3 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Мандрівники',
 };
-
-export default function TravellersPage() {
-  return <h1>Travellers Page</h1>;
-}
