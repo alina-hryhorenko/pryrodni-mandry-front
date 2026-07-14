c6d96eac0dde1820f541a4fc69b3fd92d5261a42
arstrong <arsendacenko89@gmail.com>
2026-07-14 14:37:57 +0300

    Fix generate metaData + auth

--- services/auth.ts
+++ services/auth.ts
@@ -1,1 +1,30 @@
+import { User } from '@/types/user';
+import api from './api';
+
 // Authentication API functions
+export type LoginRequest = {
+  email: string;
+  password: string;
+};
+
+export const login = async (data: LoginRequest) => {
+  const res = await api.post('/auth/login', data);
+  console.log('res.data', res.data);
+  return res.data;
+};
+
+type CheckSessionRequest = {
+  success: boolean;
+};
+
+export const checkSession = async () => {
+  const res = await api.get<CheckSessionRequest>('/auth/session');
+  console.log('checkSession', res.data);
+  return res.data.success;
+};
+
+export const getMe = async () => {
+  const { data } = await api.get<User>('/auth/me');
+  console.log('getMe', data);
+  return data;
+};
