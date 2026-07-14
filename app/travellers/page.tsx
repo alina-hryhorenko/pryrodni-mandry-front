fafe5646811c9621041f69d653f5f803041f7b70
arstrong <arsendacenko89@gmail.com>
2026-07-15 01:10:28 +0300

    traveller section

--- app/travellers/page.tsx
+++ app/travellers/page.tsx
@@ -1,9 +1,19 @@
 import type { Metadata } from 'next';
 
+import css from './TravellersPage.module.css';
+
+import TravellersSection from '@/components/travellers/TravellersSection/TravellersSection';
+
 export const metadata: Metadata = {
   title: 'Мандрівники',
 };
 
 export default function TravellersPage() {
-  return <h1>Travellers Page</h1>;
+  return (
+    <main className={css.page}>
+        <h2 className={css.title}>Мандрівники</h2>
+
+        <TravellersSection />
+    </main>
+  );
 }
---
+++ components/travellers/TravellersSection/TravellersSection.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import css from './TravellersSection.module.css';
+
+import { useEffect, useState } from 'react';
+import TravellersList from '../TravellersList/TravellersList';
+import { Traveller } from '@/types/traveller';
+import { getAllTravellers } from '@/services/users';
+import Loader from '@/components/ui/Loader/Loader';
+
+const USERS_LIMIT = 6;
+
+export default function TravellersSection() {
+  const [users, setUsers] = useState<Traveller[]>([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+
+  useEffect(() => {
+    const fetchTravellers = async () => {
+      try {
+        const data = await getAllTravellers(1, USERS_LIMIT);
+        setUsers(data.users);
+        setTotalPages(data.totalPages);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchTravellers();
+  }, []);
+
+  const handleLoadMore = async () => {
+    try {
+      setIsLoadingMore(true);
+
+      const nextPage = page + 1;
+
+      const data = await getAllTravellers(nextPage, USERS_LIMIT);
+
+      setUsers((prev) => [...prev, ...data.users]);
+      setPage(nextPage);
+    } finally {
+      setIsLoadingMore(false);
+    }
+  };
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      <TravellersList travellers={users} />
+
+      {page < totalPages && (
+        <button
+          className={css.btn}
+          onClick={handleLoadMore}
+          disabled={isLoadingMore}
+        >
+          {isLoadingMore ? 'Завантаження...' : 'Показати ще'}
+        </button>
+      )}
+    </>
+  );
+}
